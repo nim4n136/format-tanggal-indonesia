9fd6005564b873458a180d4e6a114bfbd7fe2c7d
nim4n136 <nim4n136@gmail.com>
2019-08-10 19:22:54 +0000

    Apply fixes from StyleCI

--- tests/DateFormatTest.php
+++ tests/DateFormatTest.php
@@ -1,15 +1,15 @@
 <?php
 
-use PHPUnit\Framework\TestCase;
 use Nim4n\SimpleDate;
+use PHPUnit\Framework\TestCase;
 
 final class DateFormatTest extends TestCase
-{ 
-    public function testDateEqual(){
-        
-        $dateString = "2019-02-01";
+{
+    public function testDateEqual()
+    {
+        $dateString = '2019-02-01';
         $createFormatDate = SimpleDate::date($dateString);
-        $expect = "01 Februari 2019";
+        $expect = '01 Februari 2019';
         $this->assertEquals($expect, $createFormatDate);
     }
 }
